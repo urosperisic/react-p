434dffa9ce59d0aa09e90e1db83dfdadac7022f2
urosperisic <uros2907@gmail.com>
2025-09-05 21:11:47 +0200

    009 Project Managament App - clean

---
+++ p-9/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <header>
+        <h1>re</h1>
+        <h2>ac</h2>
+        <p>t</p>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
